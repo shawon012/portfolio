275967d0b5a5d45cfa1d9cb89832d50a46b1264b
shawon012 <shawon.shakib01@gmail.com>
2023-07-03 02:02:31 +0600

    portfolio section done

---
+++ project.js
@@ -0,0 +1,38 @@
+let projectt = [
+    {
+        img:'./img/p1.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    },
+    {
+        img:'./img/p2.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    },
+    {
+        img:'./img/p3.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    },
+    {
+        img:'./img/p4.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    },
+    {
+        img:'./img/p5.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    },
+    {
+        img:'./img/p6.jpg',
+        title:'Web Development',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, alias!',
+        link:'#',
+    }
+]
--- script.js
+++ script.js
@@ -3,6 +3,27 @@ let nav = document.querySelector('nav');
 
 
 // Nav Scroll
-window.addEventListener('scroll',( )=>{
-    nav.classList.toggle('nav-scroll',window.scrollY>0)
+window.addEventListener('scroll', () => {
+    nav.classList.toggle('nav-scroll', window.scrollY > 0)
+})
+
+
+// Dynamic Project Section
+window.addEventListener('load', () => {
+    let DynamicProjects = document.querySelector('.projects');
+    DynamicProjects.innerHTML = '';
+
+    projectt.forEach(projectContent => {
+        let dynamicProject = document.createElement('article');
+        dynamicProject.className = 'project';
+        dynamicProject.innerHTML = `
+        <img src="${projectContent.img}" alt="">
+        <div class="project-shadow">
+            <h4>${projectContent.title}</h4>
+            <p>${projectContent.des}</p>
+            <a href="${projectContent.link}"> <i class="fa fa-link"></i> </a>
+        </div>
+        `
+        DynamicProjects.appendChild(dynamicProject)
+    })
 })
