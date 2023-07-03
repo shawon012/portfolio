35acab59f520674f11a6295b34c799c2a0db88c2
shawon012 <shawon.shakib01@gmail.com>
2023-07-03 13:37:33 +0600

    overall work going on

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-
-let nav = document.querySelector('nav');
-
-
+const NavLinkA = document.querySelectorAll('.nav-link a');
+const sectionS = document.querySelectorAll('section');
 // Nav Scroll
 window.addEventListener('scroll', () => {
+
+     // Add Active Class....
+     sectionS.forEach(section=>{
+        let up = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+
+        if(up>=offset && up<offset+height){
+            NavLinkA.forEach(link=>{
+                link.classList.remove('active');
+                document.querySelector('.nav-link a[href*='+ id +']').classList.add('active')
+            })
+        }
+    })
+
+    let nav = document.querySelector('nav');
     nav.classList.toggle('nav-scroll', window.scrollY > 0)
 })
 
+// Menu .........
+let open = document.querySelector('.open');
+let close = document.querySelector('.close');
+let navLink = document.querySelector('.nav-link');
+
+open.addEventListener('click',()=>{
+    navLink.style.display='block';
+    open.style.display='none';
+    close.style.display='inline-block'
+});
+close.addEventListener('click',()=>{
+    navLink.style.display='none';
+    open.style.display='inline-block';
+    close.style.display='none'
+});
+
+if(window.innerWidth < 991){
+    window.addEventListener('scroll',()=>{
+        navLink.style.display='none';
+        open.style.display='inline-block';
+        close.style.display='none'
+    });
+}
 
 // Dynamic Project Section
 window.addEventListener('load', () => {
@@ -26,4 +65,22 @@ window.addEventListener('load', () => {
         `
         DynamicProjects.appendChild(dynamicProject)
     })
-})
+});
+
+
+// Theme Mode Dark/Light.........
+let darkIcon = document.querySelector('.moon');
+let LightIcon = document.querySelector('.sun');
+
+darkIcon.addEventListener('click',()=>{
+    darkIcon.style.display='none'
+    LightIcon.style.display='inline-block'
+    document.querySelector('body').classList.add('dark-mood');
+});
+
+LightIcon.addEventListener('click',()=>{
+    LightIcon.style.display='none'
+    darkIcon.style.display='inline-block'
+    document.querySelector('body').classList.remove('dark-mood');
+});
+
